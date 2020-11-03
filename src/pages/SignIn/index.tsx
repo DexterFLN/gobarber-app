4fa380a68db72ec37a74f9d14a9d7c003530ac67
DexterFLN <dragon_dexter-x@yahoo.com.br>
2020-11-03 01:04:22 -0300

    First commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import { Image } from 'react-native';
+
+// eslint-disable-next-line import/extensions
+import Input from '../../components/Input';
+// eslint-disable-next-line import/extensions
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
